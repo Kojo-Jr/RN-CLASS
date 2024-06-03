8a768c6ef38f0ba58036b20ec4ebd2475cba4e9b
Kojo-Jr <omarieben@outlook.com>
2024-06-03 21:59:34 +0000

    update

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
-import { Text, View } from "react-native";
+import { Text, View, Image } from "react-native";
+import React from "react";
+import { FontAwesome, AntDesign } from "@expo/vector-icons";
 
 export default function App() {
+  const App = () => {
+    const height = 200;
+    const width = 200;
+  };
   return (
     <View
       style={{
@@ -11,7 +17,34 @@ export default function App() {
         alignItems: "center"
       }}
     >
-      
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          width: "12%"
+        }}
+      >
+        <View>
+          <AntDesign name="github" size={24} color="black" />
+        </View>
+
+        <View>
+          <FontAwesome name="facebook" size={24} color="black" />
+        </View>
+      </View>
+
+      <View>
+        <Image
+          source={require("./assets/mockimages/p_cat2.png")}
+          // source={{
+          // uri: "https://reactnative.dev/docs/assets/p_cat2.png"
+          // }}
+
+          style={{ width: 140, height: 140, resizeMode: "contain" }}
+        />
+      </View>
+
       <View
         style={{
           display: "flex",
@@ -53,9 +86,6 @@ export default function App() {
           </Text>
         </View>
       </View>
-
-
-
 
       <View
         style={{
